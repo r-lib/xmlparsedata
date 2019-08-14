67c3db5657843c4c369803a9266d1f1d0a5ad14d
Russell Hyde <russ_hyde@hotmail.com>
2019-08-14 13:10:56 +0100

    trim the comments for the new test

--- tests/testthat/test-debug_equals_assign.R
+++ tests/testthat/test-debug_equals_assign.R
@@ -1,42 +1,11 @@
 context("debugging `a = 1` bug in R3.6")
 
 test_that("equals_assign bug on R 3.6", {
-  # in R3.6 the xml for `a = 1` originally came out as:
-  # <exprlist>
-  #   <equal_assign ...>
-  #     <expr ...>
-  #       <SYMBOL ...>a</SYMBOL>
-  #     </expr>
-  #     <EQ_ASSIGN ...>=</EQ_ASSIGN>
-  #     <expr ...>
-  #       <NUM_CONST ...>1</NUM_CONST>
-  #     </equal_assign>
-  #   </expr>
-  # </exprlist>
-
-  # in R3.5 the same code gave
-  # ...
-  # <exprlist>
-  #   <expr ...>
-  #     <SYMBOL ...>a</SYMBOL>
-  #   </expr>
-  #   <EQ_ASSIGN ...>=</EQ_ASSIGN>
-  #   <expr ...>
-  #     <NUM_CONST ...>1</NUM_CONST>
-  #   </expr>
-  # </exprlist>
-
-  # The tags have changed:
-  # -  R3.6 intends to wrap the whole assignment statement using the tagname
-  # `equal_assign` (whereas R3.5 uses `expr`)
-
-  # But also, the xml parsing in R3.6 has interleaved the `equal_assign` tag
-  # with the final `expr` tag; resulting in an illegal xml string
-
-  # `equal_assign` nodes in results from getParseData() mean that `expr` nodes
-  # can be nested inside `equal_assign` nodes in such a way that the start of
-  # one nested-node matches the start of the equal_assign node, and the end of
-  # a nested-node matches the end of the equal_assign node.
+  # `a = 1` is an example of an R statement that gets parsed into nested xml
+  # nodes that have different token / tagnames (following the introduction of
+  # the `equal_assign` token to getParseData() in R-3.6), but the same ending
+  # position in the original code. Tokens/expressions that start before should
+  # end after any nested subexpressions in the resulting xml:
 
   xml <- xml_parse_data(parse(text = "a = 1", keep.source = TRUE))
   expect_true(is.character(xml))
