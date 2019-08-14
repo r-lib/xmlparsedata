79f44ac98f813955920efa30fdb029d7d8bbbf1b
Russell Hyde <russ_hyde@hotmail.com>
2019-08-14 13:13:59 +0100

    move the debugging test to test.R

--- tests/testthat/test-debug_equals_assign.R
+++ tests/testthat/test-debug_equals_assign.R
@@ -1,15 +1,3 @@
 context("debugging `a = 1` bug in R3.6")
 
-test_that("equals_assign bug on R 3.6", {
-  # `a = 1` is an example of an R statement that gets parsed into nested xml
-  # nodes that have different token / tagnames (following the introduction of
-  # the `equal_assign` token to getParseData() in R-3.6), but the same ending
-  # position in the original code. Tokens/expressions that start before should
-  # end after any nested subexpressions in the resulting xml:
-
-  xml <- xml_parse_data(parse(text = "a = 1", keep.source = TRUE))
-  expect_true(is.character(xml))
-  expect_true(length(xml) == 1)
-  expect_silent(x <- xml2::read_xml(xml))
-})
 
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -94,7 +94,6 @@ test_that("data frame input", {
 })
 
 
-
 test_that("Control-C character", {
   src <- "# Control-C \003
           # Bell  \007
@@ -103,5 +102,19 @@ test_that("Control-C character", {
   xml <- xml_parse_data(parse(text = src, keep.source = TRUE))
   x <- xml2::read_xml(xml)
   expect_is(x, "xml_document")
+})
+
+
+test_that("equal_assign is handled on R 3.6", {
+  # `a = 1` is an example of an R statement that gets parsed into nested xml
+  # nodes that have different token / tagnames (following the introduction of
+  # the `equal_assign` token to getParseData() in R-3.6), but the same ending
+  # position in the original code. Tokens/expressions that start before should
+  # end after any nested subexpressions in the resulting xml:
+
+  xml <- xml_parse_data(parse(text = "a = 1", keep.source = TRUE))
+  expect_true(is.character(xml))
+  expect_true(length(xml) == 1)
+  expect_silent(x <- xml2::read_xml(xml))
 })
 
