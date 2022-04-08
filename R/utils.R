93f9fe50fe2606e1650a2f9c7308138e34f597a3
Michael Chirico <michaelchirico4@gmail.com>
2022-04-08 12:15:35 -0700

    simpilfy

--- R/utils.R
+++ R/utils.R
@@ -8,14 +8,14 @@ reparse_octal <- function(pd, lines) {
   single_line <- pd$line1 == pd$line2
   out[single_line] <- substr(lines[pd$line1[single_line]], pd$col1[single_line], pd$col2[single_line])
   for (ii in which(!single_line)) {
-    if (pd$line1[ii] < pd$line2[ii] - 1L) {
-      out[ii] <- paste(
-        c(substring(lines[pd$line1[ii]], pd$col1[ii]), lines[(pd$line1[ii] + 1L):(pd$line2[ii] - 1L)], substr(lines[pd$line2[ii]], 1L, pd$col2[ii])),
-        collapse = "\n"
-      )
-    } else {
-      out[ii] <- paste0(substring(lines[pd$line1[ii]], pd$col1[ii]), "\n", substr(lines[pd$line2[ii]], 1L, pd$col2[ii]))
-    }
+    out[ii] <- paste(
+      c(
+        substring(lines[pd$line1[ii]], pd$col1[ii]),
+        if (pd$line1[ii] < pd$line2[ii] - 1L) lines[(pd$line1[ii] + 1L):(pd$line2[ii] - 1L)],
+        substr(lines[pd$line2[ii]], 1L, pd$col2[ii])
+      ),
+      collapse = "\n"
+    )
   }
   out
 }
