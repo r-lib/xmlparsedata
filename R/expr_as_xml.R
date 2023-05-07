962cfbed10a57733c4fa7afd78ef26c6a250ff9a
Michael Chirico <chiricom@google.com>
2023-05-07 13:04:19 +0000

    add a TODO comment

--- R/expr_as_xml.R
+++ R/expr_as_xml.R
@@ -12,5 +12,7 @@ expr_as_xml <- function(expr) {
   # NB: deparse() approach struggles with `{` expressions
   dput(substitute(expr), file = tmp_source)
   parsed_expr <- parse(tmp_source, keep.source = TRUE)
+  # TODO(#28): Strip the line/column metadata which
+  #   is technically 'missing' for this case.
   xml2::read_xml(xml_parse_data(parsed_expr))
 }
